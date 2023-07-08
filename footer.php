7ce310e66a03b82f273f04322d167416067d3ba0
Marvin Okafor <marvinoka4@gmail.com>
2023-07-08 18:17:39 +0100

    dragon commit

---
+++ footer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains closing divs for header.php.
+ *
+ * For more info: https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ */
+ ?>
+
+<footer class="footer text-white" role="contentinfo">
+
+  <div class="grid-container">
+    <div class="inner-footer grid-x grid-margin-x grid-margin-y text-center large-text-left">
+      <div class="small-12 medium-4 large-4 cell">
+        <h4 class="mb20">Footer Column 1</h4>
+        <p>Some text</p>
+      </div>
+
+      <div class="small-12 medium-4 large-4 cell">
+        <h4 class="mb20">Footer Column 2</h4>
+          <p>Some text</p>
+      </div>
+      <div class="small-12 medium-4 large-4 cell">
+        <h4 class="mb20">Footer Column 3</h4>
+          <p>Some text</p>
+      </div>
+
+    <div class="small-12 medium-12 large-12 cell text-center">
+        <p class="text-small">Copyright <?php echo date('Y'); ?>. All Rights Reserved. </p>
+        <p class="text-small">
+            Built for
+            <a href="https://marvinoka4.com/?utm_source=<?php echo site_url(); ?>&amp;utm_medium=website-credit&amp;utm_campaign=referral" target="_blank" title="Structured Growth">
+                <img width="20" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/fa/caret-square-o-up.svg" alt="Structured Growth"></a>
+            by <a href="https://marvinoka4.com/?utm_source=<?php echo site_url(); ?>&amp;utm_medium=website-credit&amp;utm_campaign=referral" target="_blank" title="Marvin Okafor">DragonLord</a>
+        </p>
+    </div>
+    </div>
+  </div>
+
+</footer>
+
+<div class="progress-wrap margin-bottom-2">
+    <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+        <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+    </svg>
+</div>
+
+</div>
+</div>
+
+<?php wp_footer(); ?>
+<div class="cookie"></div>
+</body>
+</html> <!-- end page -->
---
+++ parts/nav/main-nav.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The off-canvas menu uses the Off-Canvas Component
+ *
+ */
+?>
+
+<div class="off-canvas-content" data-off-canvas-content>
+    <header class="header" role="banner">
+
+        <div class="grid-container bg-light-grey fluid padding-vertical-1">
+            <div class="grid-x align-middle">
+                <div class="large-4 medium-6 small-6 cell">
+                    <a href="<?php echo home_url(); ?>" >
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/util/dragon-lord-logo.svg" width="200" />
+                    </a>
+                </div>
+                <div class="large-8 medium-6 small-6 cell flex-container align-right align-middle">
+                    <?php
+                    wp_nav_menu(
+                        array(
+                            'theme_location' => 'main-menu',
+                            'container' => '',
+                            'items_wrap' => '<ul class="dropdown menu show-for-large" data-dropdown-menu>%3$s</ul>',
+                            'walker' => new nav_walker(),
+                        )
+                    )
+                    ?>
+                    <div class="hide-for-large">
+                        <button class="hamburger hamburger--elastic" type="button" aria-label="Menu" aria-controls="navigation" data-toggle="off-canvas">
+  						<span class="hamburger-box">
+    					<span class="hamburger-inner"></span>
+  						</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </header> <!-- end .header -->
+
