4026c502008810f76b56a080c0546b77d444c968
Marvin Okafor <marvinoka4@gmail.com>
2025-03-23 13:48:54 +0100

    revert commit

--- footer.php
+++ footer.php
@@ -12,44 +12,6 @@
 <footer class="footer text-white" role="contentinfo">
 
   <div class="grid-container">
-<<<<<<< HEAD
-    <div class="inner-footer grid-x grid-margin-x grid-margin-y text-center align-center-middle padding-vertical-4">
-
-      <div class="cell large-12 medium-12 small-12 padding-bottom-2 align-center text-center">
-        <a href="/">
-          <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/util/site_logo_white.svg" width="250" alt="AKM" />
-        </a>
-      </div>
-
-      <div class="small-12 medium-10 large-10 cell text-center">
-        <p class="text-small">Mortgage “N” Protect is an independent website managed by Alphakey Mortgages. By submitting your details, you agree to them being used by Alphakey Mortgages - a trading style of ITM Capital Limited, an Appointed Representative of Cornerstone Finance Group Ltd – a firm authorised and regulated by The Financial Conduct Authority No.767202.</p>
-        <p class="text-small">Your initial consultation and any quotations are free, and you are under no obligation to proceed with any options that may be available to you. Please note that commercial mortgages and some buy-to-let mortgages are not FCA-regulated products.</p>
-        <p class="text-small">Think carefully before securing other debts against your home. If you do not keep up repayments on your mortgage, your home may be repossessed.</p>
-      </div>
-
-      <div class="small-12 medium-12 large-12 cell text-center">
-        <ul class="menu align-center">
-          <li><a href="#" style="color: white">
-              <p class="text-small">Contact Us</p>
-            </a></li>
-          <li><a href="#" style="color: white">
-              <p class="text-small">Privacy Policy</p>
-            </a></li>
-          <li><a href="#" style="color: white">
-              <p class="text-small">Cookie Policy</p>
-            </a></li>
-          <li><a href="#" style="color: white">
-              <p class="text-small">Terms & Conditions</p>
-            </a></li>
-        </ul>
-      </div>
-
-      <div class="small-12 medium-12 large-12 cell text-center">
-        <p class="text-small">Copyright <?php echo date('Y'); ?>. All Rights Reserved.</p>
-      </div>
-
-
-=======
     <div class="inner-footer grid-x grid-margin-x grid-margin-y text-center large-text-left">
       <div class="small-12 medium-4 large-4 cell">
         <h4 class="mb20">Footer Column 1</h4>
@@ -58,23 +20,22 @@
 
       <div class="small-12 medium-4 large-4 cell">
         <h4 class="mb20">Footer Column 2</h4>
-          <p>Some text</p>
+        <p>Some text</p>
       </div>
       <div class="small-12 medium-4 large-4 cell">
         <h4 class="mb20">Footer Column 3</h4>
-          <p>Some text</p>
+        <p>Some text</p>
       </div>
 
-    <div class="small-12 medium-12 large-12 cell text-center">
+      <div class="small-12 medium-12 large-12 cell text-center">
         <p class="text-small">Copyright <?php echo date('Y'); ?>. All Rights Reserved. </p>
         <p class="text-small">
-            Built for
-            <a href="https://marvinoka4.com/?utm_source=<?php echo site_url(); ?>&amp;utm_medium=website-credit&amp;utm_campaign=referral" target="_blank" title="Structured Growth">
-                <img width="20" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/fa/caret-square-o-up.svg" alt="Structured Growth"></a>
-            by <a href="https://marvinoka4.com/?utm_source=<?php echo site_url(); ?>&amp;utm_medium=website-credit&amp;utm_campaign=referral" target="_blank" title="Marvin Okafor">DragonLord</a>
+          Built for
+          <a href="https://marvinoka4.com/?utm_source=<?php echo site_url(); ?>&amp;utm_medium=website-credit&amp;utm_campaign=referral" target="_blank" title="Structured Growth">
+            <img width="20" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/fa/caret-square-o-up.svg" alt="Structured Growth"></a>
+          by <a href="https://marvinoka4.com/?utm_source=<?php echo site_url(); ?>&amp;utm_medium=website-credit&amp;utm_campaign=referral" target="_blank" title="Marvin Okafor">DragonLord</a>
         </p>
-    </div>
->>>>>>> parent of fb4b720 (site-build)
+      </div>
     </div>
   </div>
 
